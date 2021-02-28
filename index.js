4882c62c47c9edc80b10bde03a2e4455cd13c17f
John Fuquene <jfuquene@email.wm.edu>
2021-02-28 17:39:01 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// writeCards = (["john", "juan", "sophia", "francisco"])
+
+// function sayHiToDavid(writeCards) {
+// for(let i = 0; i < writeCards.length; i++)
+//     console.log(`Whats up David, my name is ${writeCards[i]} thanks for having us!`)
+// }
+
+// sayHiToDavid(writeCards)
+
+function countDown(number){
+    n = number
+    while(n >= 0) {
+        console.log(n);
+
+        n--;
+    }
+
+
+}
+
+countDown(13)
